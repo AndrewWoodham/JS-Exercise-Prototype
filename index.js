61254bf7b5379c57691082aa5108532ff65ab289
Andrew Woodham <andrewwoodham3@gmail.com>
2020-01-21 19:37:46 -0600

    MVP complete and first stretch task complete

--- index.js
+++ index.js
@@ -86,6 +86,18 @@ function Car(model, milesPerGallon) {
 
 }
 
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance){
+  this.odometer += distance;
+
+  this.tank -= distance/this.milesPerGallon;
+
+
+}
+
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -93,15 +105,24 @@ function Car(model, milesPerGallon) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
+  
 }
+
+Baby.prototype = Object.create(Person.prototype)
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
+}
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
+  1. this does not refer to the function itself, rather it refers to the context of the execution
   2. 
   3. 
   4. 
